7512e0e9d9976ae172d6832150682e3c49d8b859
Babak <babakhanibabak@gmail.com>
2024-12-12 18:49:27 +0330

    Create DataSource for flavors

---
+++ app/src/main/java/com/example/cupcake/data/DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.cupcake.data
+
+import com.example.cupcake.R
+
+object DataSource {
+    val flavors= listOf(
+        R.string.vanilla,
+        R.string.chocolate,
+        R.string.red_velvet,
+        R.string.salted_caramel,
+        R.string.coffee
+    )
+}
