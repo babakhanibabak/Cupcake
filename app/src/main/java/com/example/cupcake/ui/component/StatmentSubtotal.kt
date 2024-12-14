370f684430592bfcb3d117c4dc1cf07bed270b7c
Babak <babakhanibabak@gmail.com>
2024-12-14 17:07:47 +0330

    Edit StatmentSubtotal.kt

--- app/src/main/java/com/example/cupcake/ui/component/StatmentSubtotal.kt
+++ app/src/main/java/com/example/cupcake/ui/component/StatmentSubtotal.kt
@@ -2,25 +2,24 @@ package com.example.cupcake.ui.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.NotificationCompat.Style
+import com.example.cupcake.R
 import com.example.cupcake.ui.theme.CupcakeTheme
-import org.w3c.dom.Text
+import java.util.Locale
 
 @Composable
 fun StatementSubtotal(
     modifier: Modifier = Modifier,
-    subtotal: Double = 24.00
+    subtotal: String = "24"
 ) {
     Row(
         modifier = modifier
@@ -29,13 +28,13 @@ fun StatementSubtotal(
         horizontalArrangement = Arrangement.End
     ) {
         Text(
-            modifier = Modifier.padding(end = 6.dp),
-            text = "Subtotal",
+            modifier = Modifier.padding(end = 12.dp),
+            text = stringResource(id = R.string.Subtotal),
             style = MaterialTheme.typography.bodyLarge,
             fontSize = 25.sp
         )
         Text(
-            text = "$${String.format("%.2f", subtotal)}",
+            text = "$${String.format(Locale("%.2f"), subtotal)}",
             style = MaterialTheme.typography.bodyLarge,
             fontSize = 25.sp
         )
