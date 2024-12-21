bb6490d434e33c304f2b08f8628413b749d3a319
Babak <babakhanibabak@gmail.com>
2024-12-21 19:10:49 +0330

    Complete MainScreen

--- app/src/main/java/com/example/cupcake/ui/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/ui/MainActivity.kt
@@ -1,27 +1,22 @@
-package com.example.cupcake
+package com.example.cupcake.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcake.ui.flavor.FlavorScreen
+import com.example.cupcake.ui.navigation.RootNavHost
 import com.example.cupcake.ui.theme.CupcakeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CupcakeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-
-                }
+                RootNavHost()
             }
         }
     }
