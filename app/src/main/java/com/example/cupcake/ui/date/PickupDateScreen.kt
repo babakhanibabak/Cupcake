60500cdd1c22eeb7e80c5844d8694ee9ee509974
Babak <babakhanibabak@gmail.com>
2024-12-29 21:25:48 +0330

    Edit PickUpDateScreen

--- app/src/main/java/com/example/cupcake/ui/date/PickupDateScreen.kt
+++ app/src/main/java/com/example/cupcake/ui/date/PickupDateScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,31 +20,38 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.cupcake.R
+import com.example.cupcake.ui.OrderUiState
 import com.example.cupcake.ui.component.CupCakeAppBar
 import com.example.cupcake.ui.component.CupcakeScreensTitles
 import com.example.cupcake.ui.component.MyButtons
 import com.example.cupcake.ui.component.RadioGroup2
 import com.example.cupcake.ui.component.StatementSubtotal
 import com.example.cupcake.ui.component.pickDate
+import com.example.cupcake.ui.main.MainViewModel
 import com.example.cupcake.ui.navigation.BaseRoute
 import com.example.cupcake.ui.theme.CupcakeTheme
 
 @Composable
 fun PickUpDateScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    viewModel: MainViewModel= hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsState()
     PickUpDateScreenContent(
         onCancelClick = { navController.navigate(BaseRoute.MainScreen) },
         onNextClick = { navController.navigate(BaseRoute.OrderScreen) },
-        onNavigateUp = {navController.navigateUp()}
+        onNavigateUp = {navController.navigateUp()},
+        uiState = uiState
     )
 }
 
 @Composable
 fun PickUpDateScreenContent(
     modifier: Modifier = Modifier,
+    uiState: OrderUiState,
     onNextClick: () -> Unit = {},
     onCancelClick: () -> Unit = {},
     onNavigateUp: () -> Unit ={},
@@ -69,7 +77,7 @@ fun PickUpDateScreenContent(
             onOptionSelected = { selectedOption.value = it }
         )
         HorizontalDivider(thickness = 1.dp)
-        StatementSubtotal(modifier = Modifier.padding(top = 16.dp))
+        StatementSubtotal(modifier = Modifier.padding(top = 16.dp), uiState =uiState)
         Row(
             modifier = Modifier
                 .padding(top = 350.dp)
@@ -101,6 +109,6 @@ fun PickUpDateScreenContent(
 @Composable
 private fun PickUpDateScreenPreview() {
     CupcakeTheme {
-        PickUpDateScreenContent()
+        PickUpDateScreenContent(uiState = OrderUiState())
     }
 }
