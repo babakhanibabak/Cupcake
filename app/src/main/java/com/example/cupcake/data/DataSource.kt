13e31b1ec9b33d194bbe07aeb7b3fb10c9361d37
Babak <babakhanibabak@gmail.com>
2024-12-31 01:17:59 +0330

    Edit DataSource

--- app/src/main/java/com/example/cupcake/data/DataSource.kt
+++ app/src/main/java/com/example/cupcake/data/DataSource.kt
@@ -1,13 +1,22 @@
 package com.example.cupcake.data
 
+import androidx.core.util.Pair
 import com.example.cupcake.R
 
 object DataSource {
-    val flavors= listOf(
+    val flavors = listOf(
         R.string.vanilla,
         R.string.chocolate,
         R.string.red_velvet,
         R.string.salted_caramel,
         R.string.coffee
     )
+
+    val quantityOptions = listOf(
+        Pair(R.string.One_cupcake, 1),
+        Pair(R.string.Six_cupcake, 6),
+        Pair(R.string.Twelve_cupcakes, 12)
+
+    )
 }
+
