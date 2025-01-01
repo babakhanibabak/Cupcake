147a351b3ff4c79fc4df6beca5f5a936cd68c856
Babak <babakhanibabak@gmail.com>
2025-01-01 23:57:35 +0330

    Edit MainViewModel

--- app/src/main/java/com/example/cupcake/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/cupcake/ui/main/MainViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.cupcake.ui.main
 
 import androidx.lifecycle.ViewModel
-import com.example.cupcake.data.OrderUiState
+import com.example.cupcake.ui.OrderUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
